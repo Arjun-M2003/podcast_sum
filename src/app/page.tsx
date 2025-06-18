4971e31707124b1b3698d1a6682f7efe71f0046a
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-06-18 15:29:49 -0400

    organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import TopDash from './TopDash';
-import Upload from './upload';
+import TopDash from './components/TopDash';
+import Upload from './components/upload';
 
 export default function Home() {
   return (
